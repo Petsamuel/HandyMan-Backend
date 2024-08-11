307edc288bc355130fbfa94fa1207a3f51a820cb
petsamuel <petsamuel4@gmail.com>
2024-08-11 18:04:04 +0100

    added cors

--- app/database.py
+++ app/database.py
@@ -7,18 +7,17 @@ load_dotenv();
 
 
 
-# DATABASE_URL = os.getenv("POSTGRES_URL")
-# POSTGRES_USER = os.getenv("POSTGRES_USER")
-# POSTGRES_HOST = os.getenv("POSTGRES_HOST")
-# POSTGRES_DATABASE = os.getenv("POSTGRES_DATABASE")
-# POSTGRES_PASSWORD=os.getenv("POSTGRES_PASSWORD")
-# POSTGRES_URL_NO_SSL = os.getenv("POSTGRES_URL_NO_SSL")
-# DB_url = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{5432}/{POSTGRES_DATABASE}"
+# DATABASE_URL = os.getenv("DATABASE_URL")
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD=os.getenv("POSTGRES_PASSWORD")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+POSTGRES_DATABASE = os.getenv("POSTGRES_DATABASE")
+DB_url = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{5432}/{POSTGRES_DATABASE}"
 
-database = Database(POSTGRES_URL)
+database = Database(DB_url)
 metadata = MetaData()
 engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
+    DB_url, connect_args={"check_same_thread": False}
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
