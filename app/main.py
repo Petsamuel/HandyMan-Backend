84773ee7d3ca6858c2615ecf8789f0097d58815c
petsamuel <petsamuel4@gmail.com>
2024-08-12 11:53:23 +0100

    intials

--- app/main.py
+++ app/main.py
@@ -60,7 +60,7 @@ app.include_router(payment.router)
 
 
 
-if __name__ == "__main__":
-    uvicorn.run("main:app")
+# if __name__ == "__main__":
+#     uvicorn.run("main:app", host="127.0.0.1", port=8000,  reload=True)
 
 
